976b94ad0c60a99b70d73c0f753ef8220bbc2564
orange star <ctc5000@gmail.com>
2026-03-27 18:16:44 +0400

    fix metrix

--- modules/Astropsychology/Services/AstropsychologyService.js
+++ modules/Astropsychology/Services/AstropsychologyService.js
@@ -877,7 +877,23 @@ class AstropsychologyService {
                 .planet-list { display: grid; grid-template-columns: repeat(2, 1fr); gap: 5mm; }
                 .footer { margin-top: 10mm; text-align: right; font-size: 10pt; color: #666; }
             </style>
-        </head>
+        
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107722589', 'ym');
+
+    ym(107722589, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/107722589" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</head>
+
+
         <body>
             <div class="container">
                 <div class="title-page">
